2572db207d5bf544ec0873ea9ab7c0850fc69fe7
Daniel <schindlerdaniel@web.de>
2022-11-08 12:06:59 +0100

    let Zuweisungen

--- script_01.js
+++ script_01.js
@@ -10,3 +10,14 @@
 
 // Konsolen-Ausgaben
 // console.log("Hallo");
+// console.log(firstName);
+
+/********* 02 Deklarationen + Wertzuweisung I *********/ 
+
+let firstName = "Dan";
+// firstName = 55+88;
+
+let familyName = "Schin";
+// familyName = 3;
+
+console.log(firstName, familyName);
