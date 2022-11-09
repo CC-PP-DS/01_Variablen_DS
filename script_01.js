3d439d20fe90bc18394dce4bfb3c225e8a1e952d
Daniel <schindlerdaniel@web.de>
2022-11-09 09:13:45 +0100

    typeof

--- script_01.js
+++ script_01.js
@@ -14,11 +14,20 @@
 
 /********* 02 Deklarationen + Wertzuweisung I *********/ 
 
-let firstName = "Dan";
-firstName = prompt("Bitte Vornamen eingeben:");
-// firstName = 55+88;
+// let firstName = "Dan";
+// firstName = prompt("Bitte Vornamen eingeben:");
+// // firstName = 55+88;
 
-let familyName = prompt("Bitte Nachnamen eingeben:");
-// familyName = 3;
+// let familyName = prompt("Bitte Nachnamen eingeben:");
+// // familyName = 3;
 
-console.log(firstName, familyName);
+// console.log(firstName, familyName);
+
+let test;
+test = "Hai";
+// test = 2;
+// test = true;
+
+//* JS ist eine untypisierte Sprache! | untyped *
+console.log("Datentyp: ", typeof test);
+console.log("Inhalt: ", test);
